0c7d72ddc575bfe1fe74ccd50e156fc8f9432e6f
Peterson A Sena <petersonsena@gmail.com>
2024-06-11 22:51:11 +0200

    Add info content

--- scripts.js
+++ scripts.js
@@ -28,34 +28,21 @@ document.addEventListener("DOMContentLoaded", function() {
     updateClock();
     setInterval(updateClock, 60000);
 
-   
-
-    function simulateDownload() {
-        if (progress < totalMB) {
-            const speed = Math.random() * 2 + 1; // Simula una velocidad de 1 a 3 MB/s
-            progress += speed;
-            if (progress > totalMB) {
-                progress = totalMB;
-            }
-            speedElement.textContent = speed.toFixed(2) + ' MB/s';
-            downloadedElement.textContent = progress.toFixed(2) + ' MB';
-            progressFill.style.width = (progress / totalMB * 100) + '%';
-
-            setTimeout(simulateDownload, 1000);
-        } else {
-            speedElement.textContent = '0 KB/s';
-            closeWindow();
-        }
-    }
 
     function closeWindow() {
         const downloadWindow = document.getElementById('download-window');
         downloadWindow.style.display = 'none';
     }
 
-    simulateDownload();
-    
+       
 });
+
+function downloadCV() {
+    const downloadLink = document.createElement("a");
+    downloadLink.href = "docs/CV_Peterson_Sena.pdf";
+    downloadLink.download = "CV_Peterson_Sena.pdf";
+    downloadLink.click();
+}
 
 function showSection(sectionId) {
     const sections = document.querySelectorAll('.section');
@@ -146,7 +133,138 @@ function openProjectModal(projectId) {
         modal.style.display = 'flex';
     }
 }
+const aboutContent = `
+<div class="about-container">
+    <div class="about-photo">
+        <img src="img/petercover.jpg" alt="Foto de Perfil">
+    </div>
+    <div class="about-details">
+        <button class="download-cv" onclick="downloadCV()">Descargar CV</button>
+        <p>Email: <a href="mailto:petesonsena24@gmail.com">petesonsena24@gmail.com</a></p>
+        <p>Teléfono: <a href="tel:+34695469001">+34695469001</a></p>
+        <p>Programador PHP con experiencia previa como creador de videoclips musicales. Me reinventé hacia la programación, un mundo que siempre me ha cautivado desde mi primer encuentro con MS-DOS en 1995. Fusiono mi experiencia en la creación audiovisual con la programación para desarrollar soluciones innovadoras en este emocionante campo. Estoy siempre abierto a conocer nuevas tecnologías y aprender nuevas formas de lograr un código limpio y consistente. Disfruto sumergirme en desafios, son oportunidades para crecer y superarme constamente</p>
+    </div>
+</div>
+`;
+document.getElementById('about').innerHTML = aboutContent;
+
+const experienceContent ={ 
+experience:`
+<div class="experience-grid">
+    <div class="experience-item">
+        <div class="experience-summary">Programador Backend PHP</div>
+        <div class="experience-center">IT Academy- Barcelona Activa</div>
+        <div class="experience-date">09/23 - 03/24</div>
+        <div class="experience-skills">
+        <p><strong>Habilidades aprendidas:</strong></p>
+        <ul>
+            <li>Scrum, Swagger</li>
+            <li>UniTests, PHPUnit</li>
+            <li>GitActions</li>
+        </ul>
+        <p><strong>Habilidades blandas:</strong></p>
+        <ul>
+            <li>Comunicación efectiva</li>
+            <li>Gestión del tiempo</li>
+            <li>Resolución de problemas</li>
+            <li>Creatividad</li>
+            <li>Trabajo en equipo</li>
+            <li>Adaptabilidad</li>
+            <li>Atención al detalle</li>
+            <li>Pensamiento crítico</li>
+            <li>Capacidad de aprendizaje continuo</li>
+        </ul>
+    </div>
+    </div>
+    <div class="experience-item">
+    <div class="experience-summary">Filmmaker </div>
+    <div class="experience-center">FreeLance</div>
+    <div class="experience-date">09/15 - 03/20</div>
+    <div class="experience-skills">
+      <p><strong>Habilidades blandas:</strong></p>
+<ul>
+    <li>Comunicación efectiva</li>
+    <li>Gestión del tiempo</li>
+    <li>Resolución de problemas</li>
+    <li>Creatividad</li>
+    <li>Trabajo en equipo</li>
+    <li>Adaptabilidad</li>
+    <li>Atención al detalle</li>
+</ul>
+    </div>
+</div>
+    </div>
+`};
+
+document.getElementById('experience').innerHTML = experienceContent.experience;
+
+const studiesContent = {
+    bootcamps: `
+        <div class="bootcamp-grid">
+        <div class="study-item">
+        <div class="study-summary">Bootcamp de Java </div>
+        <div class="studycenter">Alura - Oracle Next Education</div>
+        <div class="study-date">12/22 -present</div>
+        <div class="study-skills">
+            <p><strong>Habilidades aprendidas:</strong></p>
+            <ul>
+                <li>Java, OOP, PostgreSQL</li>
+                <li>Spring boot, Hibernate</li>
+                <li>Maven, IntelliJ, Eclipse</li>
+            </ul>
+        </div>
+    </div>
+            <div class="study-item">
+                <div class="study-summary">Boot Camp Full Stack PHP</div>
+                <div class="studycenter">IT Academy Barcelona Activa</div>
+                <div class="study-date">03/23 - 09/2023</div>
+                <div class="study-skills">
+                    <p><strong>Habilidades aprendidas:</strong></p>
+                    <ul>
+                        <li>PHP, Laravel, MySQL</li>
+                        <li>HTML, CSS</li>
+                        <li>Git, XAMPP, PHPMyAdmin, Composer</li>
+                    </ul>
+                </div>
+            </div>
+           
+            <div class="study-item">
+                <div class="study-summary">Edic y Postprod de audiovisuales</div>
+                <div class="studycenter">35mm School</div>
+                <div class="study-date">02/19 - 02/20</div>
+                <div class="study-skills">
+                    <p><strong>Habilidades aprendidas:</strong></p>
+                    <ul>
+                        <li>Davinci Resolve</li>
+                        <li>Adobe After Effects</li>
+                        <li>Adobe Premiere</li>
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+    `,
+    certifications: `
+    <div class="cert-grid">
+        
+    </div>
+
+
+    `
+};
+
+document.getElementById('bootcamps').innerHTML = studiesContent.bootcamps;
+document.getElementById('certifications').innerHTML = studiesContent.certifications;
 
 function closeProjectModal() {
     document.getElementById('projectModal').style.display = 'none';
 }
+window.showSection = showSection;
+window.showSubsection = showSubsection;
+window.showSubsectionSkills = showSubsectionSkills;
+window.maximizeWindow = maximizeWindow;
+window.closeWindow = closeWindow;
+window.downloadCV = downloadCV;
+window.openProjectModal = openProjectModal;
+window.closeProjectModal = closeProjectModal;
+window.toggleStartMenu = toggleStartMenu;
