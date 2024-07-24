320cfdd075cf499170ad51dcf58e2f62a4bc9b81
Peterson Andres Sena Arias <121895258+pupadevs@users.noreply.github.com>
2024-07-24 07:17:39 +0200

    Update download.js

--- js/download.js
+++ js/download.js
@@ -1,6 +1,6 @@
 function downloadCV() {
     const downloadLink = document.createElement("a");
-    downloadLink.href = "docs/Peterson_Sena_CV.pdf";
+    downloadLink.href = "docs/CV_Peterson_Sena (4).pdf";
     downloadLink.download = "CV_Peterson_Sena.pdf";
     downloadLink.click();
   }
